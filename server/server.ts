cb54659d95b2d7e945606349e807fc8a4849aac3
Raj Mhatre <rajmhatre@Rajs-Mac-mini.local>
2026-03-28 14:42:00 +0530

    updated backend server.js

--- server/server.ts
+++ server/server.ts
@@ -14,7 +14,15 @@ const DIRNAME = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use('/api', router);
 
 app.use(express.static(path.join(DIRNAME, '/client/dist')));
