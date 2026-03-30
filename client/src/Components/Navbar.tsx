11a03139d5cb9dc193afb16822d771c009fe0e74
Raj Mhatre <rajmhatre@Rajs-Mac-mini.local>
2026-03-30 11:35:49 +0530

    added cd and style chnage

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <div className="h-[60px] bg-sky-200 shadow-sm flex items-center px-6 py-2">
             <div className="flex justify-between w-full items-center">
-                <h5 className="font-semibold text-xl text-gray-500">TodoList</h5>
+                <h5 className="font-semibold text-xl text-black">TodoList</h5>
                 <div className="flex justify-center items-center md:gap-10 gap-2">
                     <div className="md:text-sm text-xs bg-yellow-200 px-5 py-1 rounded-md shadow-sm text-gray-500">
                         <p><span className="font-semibold">User:</span> {user?.username || 'Guest'}</p>
